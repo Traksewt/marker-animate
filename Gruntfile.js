4dc0f7936937daea1443de99095c58d3b54e3651
viskin <r.viskin@terikon.com>
2015-05-29 20:27:20 +0300

    Wrapped all with grunt-umd.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,33 @@ module.exports = function(grunt) {
 	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-umd');
 	
 	grunt.initConfig({
 		
 		pkg: grunt.file.readJSON('package.json'),
 		
+		umd: {
+			markerAnimate: {
+				options: {
+					src: 'markerAnimate.js',
+					dest: 'dist/markerAnimate.js',
+					deps: {
+						'default': ['jQuery'],
+						amd: ['jquery', 'jquery.easing'],
+						cjs: ['jquery', 'jquery.easing']
+					}
+				}
+			}
+		},
+
 		uglify: {
 			options: {
 				sourceMap: true,
 				banner: '/* <%= grunt.task.current.target %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> Node wrapper for marker-animate (C) 2015 Terikon Software */\n'
 			},
-			'marker-animate': {
-				src: 'markerAnimate.js',
+			markerAnimate: {
+				src: 'dist/markerAnimate.js',
 				dest: 'dist/markerAnimate.min.js'
 			}
 		},
@@ -24,5 +39,5 @@ module.exports = function(grunt) {
 		
 	});
 	
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['umd', 'uglify']);
 };
